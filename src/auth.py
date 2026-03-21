89e74a4da1f636ceeb25d2e6b36efc69f3ad11ec
Stormy <80485413+0hStormy@users.noreply.github.com>
2026-03-20 19:04:21 -0700

    Begin work on account authentication

---
+++ src/auth.py
@@ -0,0 +1,48 @@
+"""
+Rotur account authentication/linking is handled in this script.
+Made with <3 by Stormy
+"""
+
+import requests
+from enum import Enum
+
+
+class LinkedStatus(Enum):
+    LINKED = True
+    UNLINKED = False
+
+
+def get_linking_code() -> str:
+    """
+    Fetches linking code from Rotur
+
+    :return: Linking code
+    :rtype: str
+    """
+    code_url = "https://api.rotur.dev/link/code"
+    response = requests.get(code_url)
+    code = response.json()["code"]
+    return code
+
+
+def try_get_token(code: str) -> tuple[LinkedStatus, str]:
+    """
+    Attempt to get token based on Rotur linking code
+
+    :param code: Linking code
+    :type code: str
+    :return: Tuple of the linked status and user token
+    :rtype: tuple[LinkedStatus, str]
+    """
+    linking_url = f"https://api.rotur.dev/link/user?code={code}"
+    response = requests.get(linking_url)
+    is_linked = response.json()["linked"]
+
+    # Return values
+    if is_linked:
+        link_status = LinkedStatus.LINKED
+        token = response.json()["token"]
+    else:
+        link_status = LinkedStatus.UNLINKED
+        token = ""
+    return (link_status, token)
