0c68045c6c8b1c87757bf6981091fa55845972bc
Stormy <80485413+0hStormy@users.noreply.github.com>
2026-03-21 21:11:13 -0700

    Add initial account linking UI

---
+++ src/ui.py
@@ -0,0 +1,96 @@
+import gi
+import os
+import platform
+import sys
+
+gi.require_version("Gtk", "4.0")
+from gi.repository import GLib, Gtk, Gdk, Gio  # type: ignore
+
+
+class Chlorine(Gtk.Application):
+    def __init__(self):
+        super().__init__(application_id="com.xhlowi.Chlorine")
+
+    def do_activate(self):
+        """
+        UI initialization, consisting of building the UI via GtkBuilder,
+        loading custom icon theme and CSS, then presents application to user.
+        """
+
+        # Build UI from auth.ui XML
+        builder = Gtk.Builder()
+        builder.add_from_file("../ui/auth.ui")
+        win = builder.get_object("ChlorineAuth")
+
+        # Checks if GtkBuilder UI isn't broken
+        if not isinstance(win, Gtk.ApplicationWindow):
+            raise RuntimeError("ChlorineAuth doesn't exist")
+
+        # Initialize custom icon theme and CSS
+        icon_theme = Gtk.IconTheme.get_for_display(Gtk.Window().get_display())
+        icon_theme.add_search_path("../assets")
+        load_css("../ui/style.css")
+
+        # Starts application
+        win.set_application(self)
+        win.present()
+
+
+def load_css(path: str):
+    """
+    Load GTK CSS from given file patth
+
+    :param path: Path to CSS stylesheet
+    :type path: str
+    """
+    css = Gtk.CssProvider()
+    css.load_from_path(path)
+
+    display = Gdk.Display.get_default()
+
+    if not isinstance(display, Gdk.Display):
+        raise RuntimeError("CSS could not load, something is seriously wrong")
+
+    Gtk.StyleContext.add_provider_for_display(
+        display, css, Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION
+    )
+
+
+def set_system_theme():
+    """Workaround to GTK 4 forcing Adwaita by default"""
+
+    # Ignore if platform isn't Linux
+    if platform.system() != "Linux":
+        return
+
+    # Search for default theme via GIO
+    settings = Gio.Settings.new("org.gnome.desktop.interface")
+    theme = settings.get_string("gtk-theme")
+    icon_theme = settings.get_string("icon-theme")
+    os.environ["GTK_THEME"] = theme
+    os.environ["GTK_ICON_THEME"] = icon_theme
+
+    # Set font size to the system font size
+    font = settings.get_string("font-name").split()[1]
+    font_css = f"""
+    * {{
+	    font-family: system-ui;
+        font-size: {font}pt;
+    }}
+    """
+    provider = Gtk.CssProvider()
+    provider.load_from_data(font_css)
+    display = Gdk.Display.get_default()
+
+    if not isinstance(display, Gdk.Display):
+        raise RuntimeError("CSS could not load, something is seriously wrong")
+
+    Gtk.StyleContext.add_provider_for_display(
+        display, provider, Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION
+    )
+
+
+if __name__ == "__main__":
+    set_system_theme()
+    app = Chlorine()
+    app.run(sys.argv)
---
+++ ui/EDITING.md
@@ -0,0 +1,5 @@
+# Editing UI
+
+If you're going to edit the UI, please use
+[Cambalache](https://github.com/ag-python/cambalache) instead of directly
+editing the `.ui` files in the source tree.
